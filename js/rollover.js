504c6bb2620c300deb06ab2d0c8dade27353cf17
yuichi matsuoka <anything12new14@gmail.com>
2014-12-29 15:27:05 +0900

    portfolio add

--- js/rollover.js
+++ js/rollover.js
@@ -3,11 +3,11 @@
 $(function(){
 
 	//rollover
-	$('#header ul li img').rollover();
-	$('#content ul li img').rollover();
+	$('#header ul li img:not(.animate)').rollover();
+	$('#content ul li img:not(.animate)').rollover();
 
-	$('#content #photo ul li img').rollover();
+	$('#content #photo ul li img:not(.animate)').rollover();
 
-	$('#portfolio .right img').rollover();
+	$('#portfolio .right img:not(.animate)').rollover();
 
 });
